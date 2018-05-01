38bf74a8c475d49a14c73fccf14c015273bf5715
ryusoft <ryu@hermo.my>
2018-05-01 13:01:32 +0800

    add powered

---
+++ src/Alpstein.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @author RYU Chua <me@ryu.my>
+ * @link http://www.ryu.my
+ * @copyright Copyright (c) Ryusoft
+ */
+
+namespace alpstein;
+
+/**
+ * Class Alpstein
+ * @package alpstein
+ */
+class Alpstein
+{
+    /**
+     * @return string
+     */
+    public static function powered()
+    {
+        return 'Powered by <a href="https://www.alpstein.my" target="_blank" rel="external">Alpstein Software</a>';
+    }
+}
