e3acae5eb92075d099e4092104395a9200a1bcc1
ryusoft <ryu@hermo.my>
2018-07-16 13:58:25 +0800

    admin lte icheck

--- src/adminlte/assets/AdminLteAsset.php
+++ src/adminlte/assets/AdminLteAsset.php
@@ -37,7 +37,7 @@ class AdminLteAsset extends AssetBundle
         'font-awesome/css/font-awesome.min.css',
         'css/AdminLTE.min.css',
         'css/skins/_all-skins.css',
-        'plugins/iCheck/square/blue.css',
+        'plugins/iCheck/all.css',
         'yii/extra.css',
         'yii/style.css',
     ];
@@ -49,4 +49,35 @@ class AdminLteAsset extends AssetBundle
         'plugins/iCheck/icheck.min.js',
         'js/adminlte.min.js',
     ];
+
+    /**
+     * @param View $view
+     * @return mixed
+     */
+    public static function register($view)
+    {
+        $extraJs = <<<EOL
+$('input[type="checkbox"].minimal, input[type="radio"].minimal').iCheck({
+  checkboxClass: 'icheckbox_minimal-blue', radioClass: 'iradio_minimal-blue'
+});
+$('input[type="checkbox"].minimal-red, input[type="radio"].minimal-red').iCheck({
+  checkboxClass: 'icheckbox_minimal-red', radioClass: 'iradio_minimal-red'
+});
+$('input[type="checkbox"].minimal-green, input[type="radio"].minimal-green').iCheck({
+  checkboxClass: 'icheckbox_minimal-green', radioClass: 'iradio_minimal-green'
+});
+$('input[type="checkbox"].flat, input[type="radio"].flat').iCheck({
+  checkboxClass: 'icheckbox_flat-blue', radioClass: 'iradio_flat-blue'
+})
+$('input[type="checkbox"].flat-red, input[type="radio"].flat-red').iCheck({
+  checkboxClass: 'icheckbox_flat-red', radioClass: 'iradio_flat-red'
+})
+$('input[type="checkbox"].flat-green, input[type="radio"].flat-green').iCheck({
+  checkboxClass: 'icheckbox_flat-green', radioClass: 'iradio_flat-green'
+})
+EOL;
+        $view->registerJs($extraJs, View::POS_READY, 'adminlte.extra');
+
+        return parent::register($view);
+    }
 }
