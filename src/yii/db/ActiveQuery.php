afbbf1a1c1c04fa5ed873d18073713681897d6a7
RYU Chua <me@ryu.my>
2018-12-06 10:19:50 +0800

    enhance active query alias, will auto fix existing condition

--- src/yii/db/ActiveQuery.php
+++ src/yii/db/ActiveQuery.php
@@ -50,6 +50,45 @@ class ActiveQuery extends \yii\db\ActiveQuery
     }
 
     /**
+     * @inheritdoc
+     */
+    public function alias($alias)
+    {
+        $this->setAlias($alias);
+        return parent::alias($alias);
+    }
+
+    /**
+     * @param string $value
+     */
+    protected function setAlias($value)
+    {
+        $this->replaceAlias($this->where, $this->getAlias(), $value);
+        $this->__alias = $value;
+    }
+
+    /**
+     * @param string|array $where
+     * @param string $from
+     * @param string $to
+     */
+    protected function replaceAlias(&$where, $from, $to)
+    {
+        if (is_array($where)) {
+            foreach ($where as $key => &$value) {
+                if (is_array($value)) {
+                    $this->replaceAlias($value, $from, $to);
+                } elseif (is_string($key) && strpos($key, $from . '.') !== false) {
+                    $newKey = str_replace($from . '.', $to . '.', $key);
+                    $where[$newKey] = $value;
+                    unset($where[$key]);
+                }
+                unset($value);//clean up pointer reference
+            }
+        }
+    }
+
+    /**
      * @return string
      */
     protected function getAlias()
